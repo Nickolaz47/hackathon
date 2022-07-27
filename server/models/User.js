1a7ce961a7ee7413b38d6ce1d676eca1cc37c3bd
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-07-26 23:06:46 -0300

    Created User schema.

---
+++ server/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+    profileImage: String,
+    bio: String,
+    role: String,
+    subject: String,
+    institution: String,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
