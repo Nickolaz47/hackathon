2ed8efa7a9d45918e4158221d6600eb12abd0b59
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-07-27 14:50:52 -0300

    Created route to get user by id.

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -117,4 +117,22 @@ const updateUser = async (req, res) => {
   res.status(200).json(user);
 };
 
-module.exports = { register, login, getCurrentUser, updateUser };
+// Get user by id
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(mongoose.Types.ObjectId(id)).select(
+      "-password"
+    );
+    if (!user) {
+      res.status(404).json({ errors: ["Usuário não encontrado!"] });
+      return;
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(422).json({ errors: ["Usuário não encontrado!"] });
+    return;
+  }
+};
+
+module.exports = { register, login, getCurrentUser, updateUser, getUserById };
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -7,6 +7,7 @@ const {
   login,
   getCurrentUser,
   updateUser,
+  getUserById
 } = require("../controllers/UserController");
 
 // Middlewares
@@ -15,11 +16,22 @@ const authGuard = require("../middlewares/authGuard");
 const {
   userCreateValidation,
   loginValidation,
+  userUpdateValidation,
 } = require("../middlewares/userValidations");
+const { imageUpload } = require("../middlewares/imageUpload");
 
 // Routes
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
 router.get("/profile", authGuard, getCurrentUser);
+router.put(
+  "/",
+  authGuard,
+  userUpdateValidation(),
+  validate,
+  imageUpload.single("profileImage"),
+  updateUser
+);
+router.get("/:id", getUserById)
 
 module.exports = router;
