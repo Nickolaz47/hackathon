52f8e9ff602adafb1382f717e7fc572b88503501
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-07-27 11:28:25 -0300

    Created user controller.

---
+++ server/controllers/UserController.js
@@ -0,0 +1,65 @@
+const User = require("../models/User");
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+const jwtSecret = process.env.JWT_SECRET;
+
+// Generate user token
+const generateToken = (id) => {
+  return jwt.sign({ id }, jwtSecret, { expiresIn: "7d" });
+};
+
+// Register user and sign in
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Check if user exists by email
+  const user = await User.findOne({ email });
+  if (user) {
+    res.status(422).json({ errors: ["E-mail já existe no sistema."] });
+    return;
+  }
+
+  // Generate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  // Create user
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  // If user was created successfully, return the token
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ["Houve um erro, por favor tente mais tarde."] });
+  }
+
+  res.status(201).json({ _id: newUser.id, token: generateToken(newUser._id) });
+};
+
+// Sign user in
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  // Check if user exists or password matches
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    res.status(401).json({ errors: ["Credenciais inválidas!"] });
+    return;
+  }
+
+  // Return user with token
+  res.status(200).json({
+    _id: user._id,
+    profileImage: user.profileImage,
+    token: generateToken(user._id),
+  });
+};
+
+module.exports = { register, login };
