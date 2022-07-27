d557190734887af434fe256d59e1de21679da8db
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-07-27 14:12:35 -0300

    Added middleware to check if user is authenticated.

---
+++ server/middlewares/authGuard.js
@@ -0,0 +1,25 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
+
+// Function to check if the user is authenticated 
+const authGuard = async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  // Check if header has a token
+  if (!token) {
+    return res.status(401).json({ error: "Acesso negado!" });
+  }
+
+  // Check if token is valid
+  try {
+    const verified = jwt.verify(token, jwtSecret);
+    req.user = await User.findById(verified.id).select("-password")
+    next();
+  } catch (error) {
+    res.status(401).json({errors: ["Token inválido!"]});
+  }
+};
+
+module.exports = authGuard;
