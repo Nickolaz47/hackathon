1e49d4cd8dc1be2e29422211bf2b8bf5f86d7b29
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-07-27 14:50:52 -0300

    Created mentorship schema.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const router = require("./routes/Router")
 
@@ -9,9 +10,13 @@ const app = express();
 
 // Config JSON and form data response
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Solving CORS
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+// Upload directory
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // DB connection
 require("./config/db");
---
+++ server/models/Mentorship.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const mentorshipSchema = new Schema(
+  {
+    title: String,
+    subject: String,
+    description: String,
+    mentorId: mongoose.ObjectId,
+    mentorName: String,
+    desiredStudents: Number,
+    students: Array,
+    price: Number,
+    duration: Number,
+    time: String,
+    date: Date,
+  },
+  { timestamps: true }
+);
+
+const Mentorship = mongoose.model("Mentorship", mentorshipSchema);
+
+module.exports = Mentorship;
