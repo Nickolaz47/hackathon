894c4cd631e6e8345ae9204db7d7005dda766ff8
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-07-28 17:04:39 -0300

    Fixed class in Message component.

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -2,7 +2,7 @@ const Message = ({ msg, type }) => {
   return (
     <>
       {type === "success" ? (
-        <div className="rounded-pill bg-success align-middle text-white d-flex aligns-items-center justify-content-center">
+        <div className="d-flex aligns-items-center justify-content-center rounded-pill bg-success text-white pt-3 mt-3 col-md-4 offset-md-4">
           <p>{msg}</p>
         </div>
       ) : (
