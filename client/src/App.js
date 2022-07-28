fe75e5fdc3e1aecd7093c8ab50a304417d9a6c64
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-07-28 20:53:12 -0300

    Checked authentication to access some routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 // Hooks
+import { useAuth } from "./hooks/useAuth";
+import { useContext } from "react";
 // Components
 import Navbar from "./components/Navbar";
+// Contexts
+import { UserContext } from "./contexts/UserContext";
 // Pages
 import Home from "./pages/Home/Home";
 import Register from "./pages/Register/Register";
@@ -14,17 +18,33 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
+  const { user } = useContext(UserContext);
+  const { loading } = useAuth();
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div className="container">
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route
+            path="/dashboard"
+            element={user ? <Dashboard /> : <Navigate to="/login" />}
+          ></Route>
           <Route path="/mentorships" element={<Mentorships />}></Route>
           <Route path="/questions" element={<Questions />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to="/dashboard" />}
+          ></Route>
+          <Route
+            path="/register"
+            element={!user ? <Register /> : <Navigate to="/dashboard" />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </div>
