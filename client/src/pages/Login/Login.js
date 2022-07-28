7e884e1fb4522a660b8eaa67de37d50fab7d2e87
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-07-28 11:56:02 -0300

    Corrected errors with React bootstrap.

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,52 +1,45 @@
-import React from 'react'
-import { Card } from "reactstrap";
-
-
 const Login = () => {
-
-    return (
-          
-
-       <Card className='mt-md-4  container py-5  gradient-custom align-items-center p-3 bg-secondary'>
-       <h2 className='text-center title'>Efetuar Login</h2>
-        <form>
-            <table>
-                
-                <tr>
-
-                    <td colSpan={2}>
-
-                     <div>
-                            <label for="exampleInputEmail1">E-mail*</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email" required/>
-                        </div>
-                      </td>
-
-                </tr>
-                
-        <td colSpan={2}>
-        <div className="form-group">
-          <label for="exampleInputPassword1">Senha*</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Senha" required/>
-          </div>
-        </td>
-        
-        
-
-        
+  return (
+    <div className="mt-md-4  container py-5  gradient-custom align-items-center p-3 bg-secondary">
+      <h2 className="text-center title">Efetuar Login</h2>
+      <form>
+        <table>
+          <tr>
+            <td colSpan={2}>
+              <div>
+                <label for="exampleInputEmail1">E-mail*</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  placeholder="Email"
+                  required
+                />
+              </div>
+            </td>
+          </tr>
+          <td colSpan={2}>
+            <div className="form-group">
+              <label for="exampleInputPassword1">Senha*</label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="Senha"
+                required
+              />
+            </div>
+          </td>
         </table>
 
-        <center className='center-layout'>
-          <button type="submit" className="btn btn-dark align-self-center">Efetuar Login</button>
+        <center className="center-layout">
+          <button type="submit" className="btn btn-dark align-self-center">
+            Efetuar Login
+          </button>
         </center>
-
-        
       </form>
- 
-      </Card>
-
-      );
-  };
-
+    </div>
+  );
+};
 
 export default Login;
