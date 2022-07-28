206dfc31a565dc7294778c7b97e16a59c48dcb74
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-07-28 15:55:48 -0300

    Created user context.

---
+++ client/src/contexts/UserContext.js
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+export const UserContextProvider = ({ children }) => {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+
+  return (
+    <UserContext.Provider value={{ user, setUser}}>
+      {children}
+    </UserContext.Provider>
+  );
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+// Contexts
+import { UserContextProvider } from "./contexts/UserContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <UserContextProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </UserContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
