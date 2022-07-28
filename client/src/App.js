582fbfd3c1ee13d4c206e7419c5bd47832a82081
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-07-28 12:08:47 -0300

    Created routes in App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
-import './App.css';
-import Formulario from './components/Form';
-import Login from './pages/Login/Login';
-
+// Hooks
+// Components
+import Formulario from "./components/Form";
+// Pages
+import Home from "./pages/Home/Home"
+import Login from "./pages/Login/Login";
+// Router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// CSS
+import "./App.css";
 
 const App = () => {
   return (
-          <>
-            <Formulario />
-            <Login  />      
-          </>
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" component={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Formulario />
+      <Login />
+    </div>
   );
-}
+};
 
 export default App;
