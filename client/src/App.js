46f6eb9ea1d5fe2502d81a83834a5d838955fd55
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-07-28 20:26:11 -0300

    Created all routes and pages.

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home/Home";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
+import Mentorships from "./pages/Mentorships/Mentorships";
+import Questions from "./pages/Questions/Questions";
+import Dashboard from "./pages/Dashboard/Dashboard";
 // Router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // CSS
@@ -17,6 +20,9 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/mentorships" element={<Mentorships />}></Route>
+          <Route path="/questions" element={<Questions />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
         </Routes>
---
+++ client/src/pages/Mentorships/Mentorships.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mentorships = () => {
+  return (
+    <div>Mentorships</div>
+  )
+}
+
+export default Mentorships
