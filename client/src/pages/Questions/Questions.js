7bdc507a8f290dec792a97f1fd84fcf6a0461ada
JaoVit0r <joaovitor.nunes.ismart@gmail.com>
2022-07-28 23:58:43 -0300

    Beginning of questions' page

--- client/src/pages/Questions/Questions.js
+++ client/src/pages/Questions/Questions.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import axios from 'axios';
+
+const port = process.env.PORT;
+const api = `http://localhost:${port}`
+
+axios.get(api).then((res) => {
+  console.log(res)
+})
+
 
 const Questions = () => {
+  const listaPerguntas = ['pergunta1','pergunta2','pergunta3'] // depois substituir pelo array vindo do get
+
   return (
-    <div>Questions</div>
+    <div className='container-fluid'>
+      <h1>Questions</h1>
+      <ul>
+        {listaPerguntas.map((pergunta) => {
+          <li>{pergunta}</li>
+        })}
+      </ul>
+    </div>
   )
 }
 
