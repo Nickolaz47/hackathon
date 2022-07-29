ff437783c4c6325f4355aa4a10fa25499186a9d4
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-07-28 22:05:25 -0300

    Structured Dashboard page.

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useState } from "react";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [menuOption, setMenuOption] = useState("profile");
 
-export default Dashboard
+  return (
+    <div className="container">
+      <div className="row">
+        <aside className="col-md-3 btn-group-vertical my-1">
+          <button
+            className="btn btn-dark btn-lg"
+            value="profile"
+            onClick={(e) => setMenuOption(e.target.value)}
+          >
+            Perfil
+          </button>
+          <button
+            className="btn btn-dark btn-lg"
+            value="mentorships"
+            onClick={(e) => setMenuOption(e.target.value)}
+          >
+            Mentorias
+          </button>
+          <button
+            className="btn btn-dark btn-lg"
+            value="questions"
+            onClick={(e) => setMenuOption(e.target.value)}
+          >
+            Perguntas
+          </button>
+          <button
+            className="btn btn-dark btn-lg"
+            value="answers"
+            onClick={(e) => setMenuOption(e.target.value)}
+          >
+            Respostas
+          </button>
+        </aside>
+        <div className="col-md-9">
+          {}
+          {}
+          {}
+          {}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
